263acff3c51baaa35c91fdd2c4580e5b6facaf40
NaveenRayappan1808 <naveenrnm1808@gmail.com>
2024-09-01 00:26:42 +0530

    second time added app code

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ to remove the file : git rm --cached index.html / git rm --cached app.JS
 to comit the file : git commit -m "initial commit"
 to add in remote : git remote add origin copy paste that URL
 to pussh the code git push -u origin master
+
+
+to create a file -> touch index.html and app.JS
+to add a file : $git add index.html / $git add app.JS
+to check status : git status 
+to remove the file : git rm --cached index.html / git rm --cached app.JS
+to comit the file : git commit -m "initial commit"
+to add in remote : git remote add origin copy paste that URL
+to pussh the code git push -u origin master
